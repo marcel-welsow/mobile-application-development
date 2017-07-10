932e7d947c6d72b472328fe2349ec829aa1fa662
mwelsow <marcel.welsow@lidl.com>
2017-07-10 13:37:25 +0200

    finalize project

---
+++ app/src/main/java/com/mojodictive/makeyournode/model/Authentification.java
@@ -0,0 +1,8 @@
+package com.mojodictive.makeyournode.model;
+
+/**
+ * Created by welsow on 06.07.17.
+ */
+
+public class Authentification {
+}
---
+++ app/src/main/java/com/mojodictive/makeyournode/model/LoginInformation.java
@@ -0,0 +1,8 @@
+package com.mojodictive.makeyournode.model;
+
+/**
+ * Created by welsow on 07.07.17.
+ */
+
+public class LoginInformation {
+}
